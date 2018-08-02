314d2bd09eeb34b2f070302be9806ac70b73f277
wy.exe <WY.exe@hotmail.com>
2018-08-02 22:10:26 +0800

    Add project files.

---
+++ asktaoCheat/dllmain.cpp
@@ -0,0 +1,13 @@
+#include <Windows.h>
+
+BOOL WINAPI DllMain(
+	_In_ HINSTANCE hinstDLL,
+	_In_ DWORD     fdwReason,
+	_In_ LPVOID    lpvReserved)
+{
+	if (fdwReason == DLL_PROCESS_ATTACH)
+	{
+
+	}
+	return TRUE;
+}
